abed15440cba8dee6a6e0f82946672bbbc3dc950
Jingda Mai <datsmai@hotmail.com>
2020-08-22 18:58:04 +0900

    structure update

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -1,12 +1,32 @@
 package users
 
 import (
+	"github.com/JingdaMai/bookstore_items-api/domain/users"
+	"github.com/JingdaMai/bookstore_items-api/services"
+	"github.com/JingdaMai/bookstore_items-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
 func CreateUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "not implemented")
+	var user users.User
+
+	// parse request body
+	if err := c.ShouldBindJSON(&user); err != nil {
+		restErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(restErr.Status, restErr)
+		return
+	}
+
+	// create user in the service
+	result, saveErr := services.CreateUser(user)
+	if saveErr != nil {
+		c.JSON(saveErr.Status, saveErr)
+		return
+	}
+
+	// return result
+	c.JSON(http.StatusCreated, result)
 }
 
 func GetUser(c *gin.Context) {
--- services/users_service.go
+++ services/users_service.go
@@ -1,1 +1,18 @@
 package services
+
+import (
+	"github.com/JingdaMai/bookstore_items-api/domain/users"
+	"github.com/JingdaMai/bookstore_items-api/utils/errors"
+)
+
+func CreateUser(user users.User) (*users.User, *errors.RestErr) {
+	if err := user.Validate(); err != nil {
+		return nil, err
+	}
+
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
+	return &user, nil
+}
