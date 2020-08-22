151ae78297a4e3055c82d6018a984d01c5e0e05c
Jingda Mai <datsmai@hotmail.com>
2020-08-22 19:27:47 +0900

    fake dao

--- controllers/users/user_controller.go
+++ controllers/users/user_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/JingdaMai/bookstore_items-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
@@ -30,7 +31,20 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "not implemented")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+	if userErr != nil {
+		err := errors.NewBadRequestError("user id should be a number")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getErr := services.GetUser(userId)
+	if getErr != nil {
+		c.JSON(getErr.Status, getErr)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
 
 func SearchUser(c *gin.Context) {
